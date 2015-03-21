6db14093412ebf2dd87fa50ddeaa1ffba35fc44f
Stargator <gator@linuxmail.org>
2015-03-21 00:37:37 -0400

    Some changes for my own sake

---
+++ src/main/java/net/maynard/examples/templateEngine/util/Config.java
@@ -0,0 +1,7 @@
+package net.maynard.examples.templateEngine.util;
+
+public interface Config {
+    // TODO: Later pull these values from a property/config file
+    public final String varStarting = "${";
+    public final String varEnding = "}";    
+}
