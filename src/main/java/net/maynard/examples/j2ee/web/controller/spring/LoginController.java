e2fbdb9a109ffaf2c5a9823ed8bb15fa4b04d05d
Stargator <wildbug@linuxmail.org>
2016-01-06 14:54:31 -0500

    Finished Section 5.2

--- src/main/java/net/maynard/examples/j2ee/web/controller/spring/LoginController.java
+++ src/main/java/net/maynard/examples/j2ee/web/controller/spring/LoginController.java
@@ -1,5 +1,6 @@
 package net.maynard.examples.j2ee.web.controller.spring;
 
+import net.maynard.examples.j2ee.web.controller.authenticator.AuthenticationService;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
@@ -7,8 +8,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class LoginController implements Controller {
+
+    private AuthenticationService authenicator;
+
+    public void setAuthenticationService(AuthenticationService authService) {
+        this.authenicator = authService;
+    }
+
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
         throws Exception {
+        String user = request.getParameter("j_username");
+        String pass = request.getParameter("j_password");
+
+        if (authenicator.isValidLogin(user, pass)) {
+            return new ModelAndView("frontpage");
+        }
+
         return new ModelAndView("wrongpassword");
     }
 }
--- src/test/java/net/maynard/examples/j2ee/web/controller/spring/LoginControllerTest.java
+++ src/test/java/net/maynard/examples/j2ee/web/controller/spring/LoginControllerTest.java
@@ -1,27 +1,57 @@
 package net.maynard.examples.j2ee.web.controller.spring;
 
+import net.maynard.examples.j2ee.web.controller.authenticator.FakeAuthenticationService;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
 
 import static org.junit.Assert.assertEquals;
 
 public class LoginControllerTest {
 
+    private final String CORRECT_PASSWORD = "correctpassword";
+    private final String VALID_USERNAME = "validuser";
+    private final String WRONG_PASSWORD = "nosuchpassword";
+
+    private FakeAuthenticationService mock;
+    private MockHttpServletRequest request;
+    private MockHttpServletResponse response;
+
+    private LoginController loginController;
+
+    @Before
+    public void setUp() throws Exception {
+        request = new MockHttpServletRequest();
+        request.addParameter("j_username", VALID_USERNAME);
+
+        response = new MockHttpServletResponse();
+        mock = new FakeAuthenticationService();
+
+        loginController = new LoginController();
+    }
+
     @Test
     public void wrongPasswordShouldRedirectToErrorPage()
             throws Exception {
-        MockHttpServletRequest request = new MockHttpServletRequest();
-        request.addParameter("j_username", "nosuchuser");
-        request.addParameter("j_password", "nosuchpassword");
+        request.addParameter("j_password", WRONG_PASSWORD);
+        mock.addUser(VALID_USERNAME, CORRECT_PASSWORD);
 
-        MockHttpServletResponse response = new MockHttpServletResponse();
-
-        Controller c = new LoginController();
-        ModelAndView v = c.handleRequest(request, response);
+        loginController.setAuthenticationService(mock);
+        ModelAndView v = loginController.handleRequest(request, response);
 
         assertEquals("wrongpassword", v.getViewName());
+    }
+
+    @Test
+    public void validLoginForwardsToFrontPage() throws  Exception {
+        request.addParameter("j_password", CORRECT_PASSWORD);
+        mock.addUser(VALID_USERNAME, CORRECT_PASSWORD);
+
+        loginController.setAuthenticationService(mock);
+        ModelAndView v = loginController.handleRequest(request, response);
+
+        assertEquals("frontpage", v.getViewName());
     }
 }
